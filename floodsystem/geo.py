5f582da7f863c4a5bd571fd6fb7fdb3faa0c4f75
Tairan Li <tl545@cam.ac.uk>
2022-02-06 22:59:48 +0000

    feat: stations within radius (Task1C)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -16,3 +16,16 @@ def stations_by_distance(stations, p):
     for station in stations:
         distances.append((station.name, station.town, haversine(station.coord, p)))
     return sorted_by_key(distances, 2)
+
+
+def stations_within_radius(stations, centre, r):
+    stations_centre = stations_by_distance(stations, centre)
+    stations_satisfied = []
+    if r < 0:
+        raise ValueError("ValueError")
+    for i in stations_centre:
+        if i[2] <= r:
+            stations_satisfied.append(i[0])
+        else:
+            break
+    return stations_satisfied
