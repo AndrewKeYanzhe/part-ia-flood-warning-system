8cbed8f9dbbeed441bc8136659f303cd513d245e
Tairan Li <tl545@cam.ac.uk>
2022-02-07 01:27:08 +0000

    feat: typical range consistency (Task1F)

---
+++ Task1F.py
@@ -0,0 +1,11 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import *
+
+def run():
+    stations = build_station_list()
+    print(inconsistent_typical_range_stations(stations))
+
+
+if __name__ == "__main__":
+    print("*** Task 1: CUED Part IF Flood Warning System ***")
+    run()
